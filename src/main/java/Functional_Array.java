5cc9cc782ef1bc9870821c3bf55d7aa8b9fb1596
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-29 23:46:01 +0530

    Added: Sqrt

--- src/main/java/Functional_Array.java
+++ src/main/java/Functional_Array.java
@@ -2,19 +2,19 @@ package com.Bridgelabz;
 import java.util.Scanner;
 public class Functional_Array {
     public static void main(String[] args) {
-        Scanner e =new Scanner(System.in);
-        System.out.println("Enter the X1 , Y1 value to calculate the Origin");
-        System.out.println("X1 =");
-        int x2=e.nextInt();
-        System.out.println("y1 =");
-        int y2=e.nextInt();
-        distance(x2,y2);
+        Scanner q =new Scanner(System.in);
+        System.out.println("To find the roots of (a*X*X + b*X + c)");
+        System.out.println("Enter the value for a,b,c");
+        System.out.println("a:");
+        double a=q.nextDouble();
+        System.out.println("b:");
+        double b=q.nextDouble();
+        System.out.println("c:");
+        double c=q.nextDouble();
+        double delta= ( b * b )-( 4 * a * c);
+        int root1=(int) ( -b + (int)(Math.sqrt((delta))/(2*a)));
+        int root2= (int) (-b - (int)(Math.sqrt((delta))/(2*a)));
+        System.out.println("The roots are (x"+root1+") (x"+root2+")");;
     }
 
-    private static void distance(int x2, int y2) {
-        int sqr1= (int) Math.pow(2, x2);
-        int sqr2= (int) Math.pow(2, y2);
-        double result = (Math.sqrt ( sqr1 + sqr2 ) );
-        System.out.println("The Euclidean distance of the points is "+result);
-    }
 }
