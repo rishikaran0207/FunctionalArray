2a3dcbab1d43c97b0a9bd37811646a3356917fa6
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-29 23:40:15 +0530

    Added: Distanceoftwo

--- src/main/java/Functional_Array.java
+++ src/main/java/Functional_Array.java
@@ -2,31 +2,19 @@ package com.Bridgelabz;
 import java.util.Scanner;
 public class Functional_Array {
     public static void main(String[] args) {
-        Scanner arr=new Scanner(System.in);
-        System.out.println("Enter the integer to be in array:");
-        int len=arr.nextInt();
-        System.out.println("Enter a value for array:");
-        int[] ar = new int[len];
-        for(int i=0;i<len;i++)
-        {
-            ar[i]=arr.nextInt();
-        }
-        int count=0;
-        for(int i=0;i<len-2;i++) {
-            for (int j = i + 1; j < len - 1; j++)
-            {
-                for(int k=j+1;k<len;k++)
-                {
-                    if(ar[i] + ar[j] + ar[k] == 0)
-                    {
-                        System.out.println("("+ar[i]+" , "+ar[j]+" , "+ar[k]+")");
-                        count++;
-                    }
-                }
-            }
+        Scanner e =new Scanner(System.in);
+        System.out.println("Enter the X1 , Y1 value to calculate the Origin");
+        System.out.println("X1 =");
+        int x2=e.nextInt();
+        System.out.println("y1 =");
+        int y2=e.nextInt();
+        distance(x2,y2);
+    }
 
-        }
-        System.out.println("the distinct triplets equal to zero");
-        System.out.println("There are "+count+" distinct triplets");
+    private static void distance(int x2, int y2) {
+        int sqr1= (int) Math.pow(2, x2);
+        int sqr2= (int) Math.pow(2, y2);
+        double result = (Math.sqrt ( sqr1 + sqr2 ) );
+        System.out.println("The Euclidean distance of the points is "+result);
     }
 }
