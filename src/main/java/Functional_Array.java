a146868aff57a737e4f6e88b513a24384df16fab
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-29 23:26:34 +0530

    Added: 2DArray

--- src/main/java/Functional_Array.java
+++ src/main/java/Functional_Array.java
@@ -2,7 +2,29 @@ package com.Bridgelabz;
 import java.util.Scanner;
 public class Functional_Array {
     public static void main(String[] args) {
-        System.out.println("Welcome");
+        Scanner ar=new Scanner(System.in);
+        System.out.println("\nEnter the no of rows and columns:");
+        System.out.println("rows:");
+        int m =ar.nextInt();
+        System.out.println("column");
+        int n =ar.nextInt();
+        int[][] arr=new int[m][n];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.println("Enter the value of "+i+" row , "+j+" column:");
+                arr[i][j]=ar.nextInt();
+            }
+        }
+        System.out.println("\nThe value in matrix is");
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.println("\narr["+i+"]["+j+"]: "+arr[i][j]);
+            }
+        }
+
     }
 }
-
